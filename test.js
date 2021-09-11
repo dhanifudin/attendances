c42deb021de97c85c38199e7a744c0c53f513252
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2021-09-11 15:27:53 +0700

    update test

--- test.js
+++ test.js
@@ -1,15 +1,22 @@
 const glob = require("glob");
 const { countCharacters } = require("wcmd/lib/utils");
+const MINIMAL_CHARS = 1000;
 
 (async () => {
   glob("./attendances/**/*.md", (err, files) => {
     files.forEach((file) => {
       const result = countCharacters(file);
-      const words = result.lines
+      const chars = result.lines
         .map((line) => line.length)
         .reduce((l1, l2) => l1 + l2);
-      if (words < 1000) process.exit(1);
+      if (chars < MINIMAL_CHARS) {
+        console.error(
+          `${file} doesn't meet summary requirements (minimal ${MINIMAL_CHARS} chars): ${chars} chars`
+        );
+        process.exit(1);
+      }
     });
+    console.log(`All summary passed`);
     process.exit(0);
   });
 })();
